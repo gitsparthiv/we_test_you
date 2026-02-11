5bd420003a25bdeb07bc44587984887df1806161
gitsparthiv <ghoshparthiv98@gmail.com>
2026-02-11 19:53:18 +0530

    Initial commit

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+function LandingPage() {
+    return (
+      <div>
+        <h2>Tuitions Teach You. We Test You.</h2>
+        <p>
+          Chapter-wise mock tests for CBSE Class 10, 11 & 12.
+        </p>
+      </div>
+    );
+  }
+  
+  export default LandingPage;
+  
