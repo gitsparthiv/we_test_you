7c228b7d5066a3fdb62abf78784248d7a895f5cf
SPANDAN MANDAL <spandansm2018@gmail.com>
2026-03-02 19:10:28 +0530

    new Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,43 @@
-
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 
 import LandingPage from "./pages/LandingPage";
-import BookYourSeat from "./pages/BookYourSeat"; // 👈 change path if different
-import Header from "./components/Header"
-import Footer from "./components/Footer" // 👈 change path if different
-import Payment from "./pages/Payment"; // 👈 change path if different
-import ScrollToTop from "./pages/ScrollToTop"; // 👈 change path if different
+import BookYourSeat from "./pages/BookYourSeat";
+import Payment from "./pages/Payment";
 import PaymentSuccess from "./pages/PaymentSuccess.jsx";
 import PaymentFailed from "./pages/PaymentFailed.jsx";
 
+import Header from "./components/Header";
+import Footer from "./components/Footer";               // OLD footer (Landing page only)
+import Small_Footer from "./components/Small_Footer";  // NEW footer (other pages)
+import ScrollToTop from "./pages/ScrollToTop";
 
+function AppLayout() {
+  const location = useLocation();
+  const isLanding = location.pathname === "/";
+
+  return (
+    <>
+      <ScrollToTop />
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/book-seat" element={<BookYourSeat />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/payment-success" element={<PaymentSuccess />} />
+        <Route path="/payment-failed" element={<PaymentFailed />} />
+      </Routes>
+
+      {isLanding ? <Footer /> : <Small_Footer />}
+    </>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <ScrollToTop />
-      <Header />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/book-seat" element={<BookYourSeat/>} />
-        <Route path="/payment" element={<Payment />} />
-        <Route path="/payment-success" element={<PaymentSuccess />} />
-<Route path="/payment-failed" element={<PaymentFailed />} />
-      </Routes>
-      <Footer />
+      <AppLayout />
     </Router>
   );
 }
