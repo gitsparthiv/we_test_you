2fdf10cda7ed4de5de0784989f9138dd1490851e
Kallol <shortwire@gmail.com>
2026-03-07 08:33:09 +0530

    package change

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	  plugins: [react()],
+	  preview: {
+		      allowedHosts: ['203.171.240.118'], // allow your host
+		      port: 4173, // optional: specify preview port
+          host: '0.0.0.0',
+		    },
 })
